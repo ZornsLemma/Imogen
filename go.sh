d23951d060404d2027c3f829a76687a898422f6b
Steven Flintham <sgf@lemma.co.uk>
2023-11-05 13:36:48 +0000

    Tweak go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-export PYTHONPATH=$HOME/src/py8dis/py8dis
+export PYTHONPATH+=$HOME/src/py8dis/py8dis
 # TODO: Mix of acme and beebasm isn't ideal. I may just want to switch to acme later. For now I'm trying to get my head round the relocation in 'g', and py8dis's handling of this in beebasm is broken, so let's just stick with acme at least until I'm more confident I know what I want.
 python3 imogen.py > imogen.asm
 beebasm -o imogen.out -i imogen.asm -v > imogen.lst
