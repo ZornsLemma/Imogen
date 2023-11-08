bd4825f5aa54559001b2a5a7538cde669be60679
Steven Flintham <sgf@lemma.co.uk>
2023-11-08 16:52:37 +0000

    WIP

--- dataA.py
+++ dataA.py
@@ -6,7 +6,7 @@ acorn.bbc()
 
 load(0x3ad5, "orig/dataA.dat", "6502", "df027a3ac06abfed1878eaec3d2bbe5f")
 word(0x3ad7, 3)
-expr(0x3ad7, "level_handler") # TODO: rename - this is probably "post_load_initialisation_handler" or something like that
+expr(0x3ad7, "level_handler") # TODO: rename - this is probably "initialise_after_load_handler" or something like that
 expr(0x3ad9, "second_level_handler") # TODO: rename
 expr(0x3adb, "third_level_handler") # TODO: rename
 entry(get_u16_binary(0x3ad7), "level_handler")
